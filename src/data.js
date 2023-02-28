067cf6baf71b1bba58d93d62a5eeaa4e4b3a73b4
ALEXANDER ALBERTO SIGUENZA CAMPOS <alexander.siguenzac@claro.com.sv>
2023-02-28 01:18:50 -0600

    Inicio del proyecto

---
+++ src/data.js
@@ -0,0 +1,38 @@
+export const data = [
+	{
+		id: 1,
+		img: 'https://www.guatemala.com/fotos/2020/04/pupusas-guate-885x500.jpg',
+		nameProduct: 'Pupusas',
+		price: 80,
+		quantity: 1,
+	},
+	{
+		id: 2,
+		img: 'https://cdn7.kiwilimon.com/recetaimagen/2806/640x426/25790.jpg.webp',
+		nameProduct: 'Pupusas',
+		price: 50,
+		quantity: 1,
+	},
+	{
+		id: 3,
+		img: 'https://assets.bonappetit.com/photos/58e29a4f8f4c125ce35a1301/1:1/w_2240,c_limit/0417-web-pupusa-beauty.jpg',
+		nameProduct: 'Pupusas',
+		price: 90,
+		quantity: 1,
+	},
+	{
+		id: 4,
+		img: 'https://www.comedera.com/wp-content/uploads/2022/09/chocolate-caliente-venezolano.jpg',
+		nameProduct: 'chocolate',
+		price: 50,
+		quantity: 1,
+	},
+	{
+		id: 5,
+		img: 'https://upload.wikimedia.org/wikipedia/commons/4/45/A_small_cup_of_coffee.JPG',
+		nameProduct: 'cafe',
+		price: 20,
+		quantity: 1,
+	},
+	// Aquí pueden agregar más o modificar los productos existentes
+];
